cc5af787f3582945cb3c5a1b63631bcbce12a0f0
sumit <sumit@cloudzilla.com>
2015-06-02 12:57:00 +0530

    Update plugin to work with hibernate4 & grails 2.5
    * The custom cache provider implementation has been deleted, since
      it needs to be re-written based on RegionFactory, Region and
      RegionAccessStrategy api(s).

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,13 +1,16 @@
 grails.project.work.dir = 'target'
 grails.project.source.level = 1.6
 
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
 
   inherits 'global'
   log 'warn'
 
   repositories {
+    grailsHome()
     grailsCentral()
+
     mavenLocal()
     mavenCentral()
   }
@@ -16,13 +19,19 @@ grails.project.dependency.resolution = {
     compile 'c3p0:c3p0:0.9.1', {
       export = false
     }
+    // explicitly here, because of the exclusion in the hibernate4 plugin below
+    compile('net.sf.ehcache:ehcache-core:2.5.2') {
+        export = false
+    }
   }
 
   plugins {
-    build ':release:2.2.0', ':rest-client-builder:1.0.3', {
+    build(':release:3.1.1') {
       export = false
     }
-    runtime ":hibernate:$grailsVersion", {
+    runtime(":hibernate4:4.3.8.1") {
+      // conflicts with ehcache-core and is the wrong version
+      excludes "ehcache"
       export = false
     }
   }
