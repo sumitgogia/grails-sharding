69f873bf7471de57cc567e4f5c0472f3fefbacad
sumit <sumit@cloudzilla.com>
2015-06-02 23:43:53 +0530

    Explicitly use READ_COMMITTED isolation level when checking & saving Shard
    * The default isolation level in some dbs (e.g. mysql) is REPEATABLE_READ.
      In such cases, the findByShardName will not see/find a Shard instance
      already committed by another transaction (e.g. in a clustered environment)
      This would result in duplicate shard instances (with the same name) getting
      saved. (There is no unique constraint defined on shardName in Shard)

--- grails-app/conf/ShardingBootStrap.groovy
+++ grails-app/conf/ShardingBootStrap.groovy
@@ -1,17 +1,20 @@
+import org.springframework.transaction.TransactionDefinition;
+
+import com.jeffrick.grails.plugin.sharding.ShardConfig;
 import com.jeffrick.grails.plugin.sharding.Shards
 import com.jeffrick.grails.plugins.sharding.Shard
 
 class ShardingBootStrap {
     def init = { servletContext ->
 
-        Shard.withTransaction {
-            Shards.list().each { shard ->
-
+        Shard.withTransaction([isolationLevel: TransactionDefinition.ISOLATION_READ_COMMITTED]) {
+            Shards.shards.each { ShardConfig shard ->
                 // Make sure the shard is in the Shard table
                 if (!Shard.findByShardName(shard.name)) {
                     new Shard(shardName: shard.name, shardCapacity: 1000, shardUsage: 0, ratio: 0.0).save()
                 }
             }
         }
+
     }
 }
